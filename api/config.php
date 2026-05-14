bdfb394f862aed8104d36e02430e68c3a621f568
tonnyvincent <154866986+tonnyvincent@users.noreply.github.com>
2026-05-15 03:29:02 +0800

    changed password database

--- api/config.php
+++ api/config.php
@@ -5,7 +5,7 @@
 const AGRIREVISE_DB_HOST = 'localhost';
 const AGRIREVISE_DB_NAME = 'u519103142_agrirevise';
 const AGRIREVISE_DB_USER = 'u519103142_agrirevisedata';
-const AGRIREVISE_DB_PASS = 'agrirevise123!@#';
+const AGRIREVISE_DB_PASS = 'Agrirevise123!@#';
 const AGRIREVISE_DB_CHARSET = 'utf8mb4';
 
 function agrirevise_database_is_configured()
